b946dc6e8ce7cb3539e823d6c7f4c4cc1736e918
Stefan Mathis <stefan_mathis@web.de>
2026-03-01 16:33:14 +0100

    Added test for complicated units

--- tests/serialize_from_uom.rs
+++ tests/serialize_from_uom.rs
@@ -1,8 +1,11 @@
 use dyn_quantity::*;
 use serde::{Deserialize, Serialize};
 use uom::si::{
-    f64::Length,
+    electric_potential::millivolt,
+    f64::*,
     length::{kilometer, millimeter},
+    magnetic_flux_density::tesla,
+    mass_density::kilogram_per_cubic_meter,
 };
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
@@ -41,7 +44,6 @@ fn test_serialize_with_units() {
         };
 
         // Without units (standard serialization)
-
         let expected = indoc::indoc! {"
         ---
         length: 0.001
@@ -65,7 +67,7 @@ fn test_serialize_with_units() {
         assert_eq!(expected, actual);
     }
     {
-        // Nine
+        // None
         let quantities = Quantities {
             length: Length::new::<millimeter>(1.0),
             opt_length: None,
@@ -74,7 +76,6 @@ fn test_serialize_with_units() {
         };
 
         // Without units (standard serialization)
-
         let expected = indoc::indoc! {"
         ---
         length: 0.001
@@ -127,4 +128,43 @@ fn test_serialize_and_deserialize() {
         let de_quantities = serde_yaml::from_str(&ser).expect("deserialization succeeds");
         assert_eq!(ser_quantities, de_quantities);
     }
+}
+
+#[test]
+fn test_complicated_units() {
+    #[derive(Serialize, Deserialize, Debug, PartialEq)]
+    struct ComposedQuantities {
+        #[serde(
+            serialize_with = "serialize_quantity",
+            deserialize_with = "deserialize_quantity"
+        )]
+        magnetic_flux_density: MagneticFluxDensity,
+        #[serde(
+            serialize_with = "serialize_quantity",
+            deserialize_with = "deserialize_quantity"
+        )]
+        voltage: ElectricPotential,
+        #[serde(
+            serialize_with = "serialize_quantity",
+            deserialize_with = "deserialize_quantity"
+        )]
+        mass_density: MassDensity,
+    }
+    let ser_composed = ComposedQuantities {
+        magnetic_flux_density: MagneticFluxDensity::new::<tesla>(0.5),
+        voltage: ElectricPotential::new::<millivolt>(2.0),
+        mass_density: MassDensity::new::<kilogram_per_cubic_meter>(8000.0),
+    };
+    let expected = indoc::indoc! {"
+    ---
+    magnetic_flux_density: 0.5 s^-2 kg A^-1
+    voltage: 0.002 s^-3 m^2 kg A^-1
+    mass_density: 8000 m^-3 kg
+    "};
+    let actual = serialize_with_units(|| serde_yaml::to_string(&ser_composed))
+        .expect("serialization succeeds");
+    assert_eq!(expected, actual);
+
+    let de_composed = serde_yaml::from_str(&actual).expect("deserialization succeeds");
+    assert_eq!(ser_composed, de_composed);
 }
